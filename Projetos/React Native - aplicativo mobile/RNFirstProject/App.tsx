e07b1266b4aa0016984d69a9ed547dd7e3fcf380
C.Junior <110105671+Clar-Junior@users.noreply.github.com>
2023-04-23 14:52:26 -0300

    Add Style

--- Projetos/React Native - aplicativo mobile/RNFirstProject/App.tsx
+++ Projetos/React Native - aplicativo mobile/RNFirstProject/App.tsx
@@ -5,11 +5,16 @@ const HelloWorldApp = () => {
   return (
     <View
       style={{
+        backgroundColor: '#131016',
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
       }}>
-      <Text>Hello, world!</Text>
+      <Text key={1} style={{
+        fontSize: 28,
+        color: '#6b6b6b',
+        
+      }}>Hello, world!</Text>
     </View>
   );
 };
