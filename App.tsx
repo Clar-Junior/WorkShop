de593fa43468662b8c33f9c019d98ae413e52a53
C.Junior <110105671+Clar-Junior@users.noreply.github.com>
2023-04-22 21:31:50 -0300

    Correção de arquivos

---
+++ App.tsx
@@ -0,0 +1,32 @@
+import { Text, View} from 'react-native';
+
+export default function App() {
+  return(
+    <View style={{
+      backgroundColor: '#131016',
+      flex: 1,
+      paddingHorizontal: 24,
+      paddingVertical: 25
+    }}>
+
+      <Text key={1} style={{
+        color: '#FCFDFE',
+        fontSize: 24,
+        lineHeight: 28.13,
+        fontWeight:'700',
+        }}>
+          Nome do Evento
+      </Text>
+
+      <Text key={2} style={{
+          color: '#6B6B6B',
+          fontSize: 16,
+          lineHeight: 18.75,
+          fontWeight: '400',
+        }}>
+          Sábado, 22 de abril de 2023.
+      </Text>
+    
+    </View>
+  );
+};
